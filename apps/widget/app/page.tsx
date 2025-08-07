a75f9502eb6674b9f57822f75d7f7b0733cd9e6e
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2025-08-07 07:19:57 +0530

    06: AI Voice Assitant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,28 +1,34 @@
 "use client";
 
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
-  const users = useQuery(api.users.getUsers, {});
-  const addUser = useMutation(api.users.addUser);
+  const {
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi();
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex items-center justify-center min-h-svh max-w-md mx-auto w-full gap-2">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Widget</h1>
-        {users?.map((user) => (
-          <div key={user._id}>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-          </div>
-        ))}
-        <Button size="sm" onClick={() => addUser({})}>
-          Add User
+
+        <Button size="sm" onClick={() => startCall()}>
+          Start Call
         </Button>
+        <Button variant={"destructive"} size="sm" onClick={() => endCall()}>
+          End Call
+        </Button>
+
+        <p>isConnected: {`${isConnected}`}</p>
+        <p>isConnecting: {`${isConnecting}`}</p>
+        <p>isSpeaking: {`${isSpeaking}`}</p>
+        <p>{JSON.stringify(transcript, null, 2)}</p>
       </div>
     </div>
   );
