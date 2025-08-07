d05acad319e135933a917eedf82a68389be915f4
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2025-08-07 12:50:58 +0530

    08: Theme

--- apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -118,6 +118,10 @@ export const DashboardSidebar = () => {
                     asChild
                     tooltip={item.title}
                     isActive={isActive(item.url)}
+                    className={cn(
+                      isActive(item.url) &&
+                        "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90"
+                    )}
                   >
                     <Link href={item.url}>
                       <item.icon className="size-4" />
@@ -141,6 +145,10 @@ export const DashboardSidebar = () => {
                     asChild
                     tooltip={item.title}
                     isActive={isActive(item.url)}
+                    className={cn(
+                      isActive(item.url) &&
+                        "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90"
+                    )}
                   >
                     <Link href={item.url}>
                       <item.icon className="size-4" />
@@ -164,6 +172,10 @@ export const DashboardSidebar = () => {
                     asChild
                     tooltip={item.title}
                     isActive={isActive(item.url)}
+                    className={cn(
+                      isActive(item.url) &&
+                        "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90"
+                    )}
                   >
                     <Link href={item.url}>
                       <item.icon className="size-4" />
