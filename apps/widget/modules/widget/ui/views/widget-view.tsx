18a28c7a02ce5fb7cb04552c647d88f8234a0ae6
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2025-08-08 15:13:03 +0530

    11: Widget Screen Router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { WidgetFooter } from "@/modules/widget/ui/components/widget-footer";
-import { WidgetHeader } from "@/modules/widget/ui/components/widget-header";
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
+import { WidgetScreen } from "@/modules/widget/types";
 
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents: Record<WidgetScreen, React.ReactNode> = {
+    error: <p>TODO: Error Screen</p>,
+    auth: <WidgetAuthScreen />,
+    chat: <p>TODO: Chat Screen</p>,
+    voice: <p>TODO: Voice Screen</p>,
+    inbox: <p>TODO: Inbox Screen</p>,
+    contact: <p>TODO: Contact Screen</p>,
+    loading: <p>TODO: Loading Screen</p>,
+    selection: <p>TODO: Selection Screen</p>,
+  };
+
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      <WidgetFooter />
+      {screenComponents[screen]}
     </main>
   );
 };
